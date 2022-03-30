794e19ac42eb35278a79ac777aef2e034e8e70d1
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2022-03-29 17:14:50 -0700

    revised test_paths.h creation

--- IDE/Espressif/ESP-IDF/setup_win.bat
+++ IDE/Espressif/ESP-IDF/setup_win.bat
@@ -31,13 +31,13 @@ if NOT EXIST "user_settings.h" (
 :: see if there was a parameter passed for a specific EDP-IDF directory
 :: this may be different than the standard ESP-IDF environment (e.g. VisualGDB)
 if not "%1" == "" (
-	if not exist "%1" (
-		echo "ERROR: optional directory was specified, but not found: %1"
-		goto :ERR
-	)
+    if not exist "%1" (
+        echo "ERROR: optional directory was specified, but not found: %1"
+        goto :ERR
+    )
 
-	SET IDF_PATH=%1
-	echo Using specified IDF_PATH: %IDF_PATH%
+    SET IDF_PATH=%1
+    echo Using specified IDF_PATH: %IDF_PATH%
 )
 
 :: if no IDF_PATH is found, we don't know what to do. Go exit with error.
@@ -151,10 +151,10 @@ if exist %WOLFSSLLIB_TRG_DIR% (
     if exist %WOLFSSLLIB_TRG_DIR% (
         SET COPYERROR=true
         echo;
-	    echo WARNING: Failed to remove %WOLFSSLLIB_TRG_DIR%
-	    echo;
-	    echo Check permissions, open files, read-only attributes, etc.
-	    echo;
+        echo WARNING: Failed to remove %WOLFSSLLIB_TRG_DIR%
+        echo;
+        echo Check permissions, open files, read-only attributes, etc.
+        echo;
     )
     echo;
 ) else (
@@ -201,9 +201,12 @@ echo Copying files to %WOLFSSLLIB_TRG_DIR%\wolfcrypt\test\
 xcopy %BASEDIR%\wolfcrypt\test                               %WOLFSSLLIB_TRG_DIR%\wolfcrypt\test\             /S /E /Q /Y
 if %errorlevel% NEQ 0 SET COPYERROR=true
 
-rem Copy dummy test_paths.h to handle the case configure hasn't yet executed
-echo F |xcopy %WOLFSSL_ESPIDFDIR%\dummy_test_paths.h         %WOLFSSLLIB_TRG_DIR%\wolfcrypt\test\test_paths.h /S /E /Y
-xcopy %WOLFSSL_ESPIDFDIR%\dummy_test_paths.h                 %WOLFSSLIB_TRG_DIR%\wolfcrypt\test\test_paths.h  /S /E /Y
+:: Copy dummy test_paths.h to handle the case configure hasn't yet executed
+echo;
+echo Copying dummy_test_paths.h to %WOLFSSLLIB_TRG_DIR%\wolfcrypt\test\test_paths.h
+echo new config                                            > %WOLFSSLLIB_TRG_DIR%\wolfcrypt\test\test_paths.h
+if %errorlevel% NEQ 0 SET COPYERROR=true
+xcopy %WOLFSSL_ESPIDFDIR%\dummy_test_paths.h                 %WOLFSSLLIB_TRG_DIR%\wolfcrypt\test\test_paths.h  /S /E /Y
 if %errorlevel% NEQ 0 SET COPYERROR=true
 
 echo;
@@ -389,7 +392,7 @@ echo REMINDER: Ensure any wolfSSL #include definitions occur BEFORE include file
 echo;
 if "%COPYERROR%" == "true" (
     echo;
-	echo WARNING: Copy completed with errors! Check for files in use, permissions, symbolic links, etc.
+    echo WARNING: Copy completed with errors! Check for files in use, permissions, symbolic links, etc.
     echo;
 )
 echo setup_win.bat for ESP-IDF completed.
