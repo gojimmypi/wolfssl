1875c781c3af9eca55794e0b2c8fd3b295770b94
gojimmypi <jim@wolfssl.com>
2023-01-19 18:14:13 -0800

    refresh local project SHA from IDF at build time

--- IDE/Espressif/ESP-IDF/refresh_sha_from_idf.bat
+++ IDE/Espressif/ESP-IDF/refresh_sha_from_idf.bat
@@ -2,5 +2,8 @@
 copy C:\SysGCC\esp32-8.4\esp-idf\v4.4.1\components\wolfssl\wolfcrypt\src\sha512.c                         C:\workspace\wolfssl-gojimmypi\wolfcrypt\src\sha512.c
 copy C:\SysGCC\esp32-8.4\esp-idf\v4.4.1\components\wolfssl\wolfcrypt\src\sha256.c                         C:\workspace\wolfssl-gojimmypi\wolfcrypt\src\sha256.c
 copy C:\SysGCC\esp32-8.4\esp-idf\v4.4.1\components\wolfssl\wolfcrypt\src\sha.c                            C:\workspace\wolfssl-gojimmypi\wolfcrypt\src\sha.c
+copy C:\SysGCC\esp32-8.4\esp-idf\v4.4.1\components\wolfssl\wolfcrypt\src\port\Espressif\esp32_util.c      C:\workspace\wolfssl-gojimmypi\wolfcrypt\src\port\Espressif\esp32_util.c
 copy C:\SysGCC\esp32-8.4\esp-idf\v4.4.1\components\wolfssl\wolfcrypt\src\port\Espressif\esp32_sha.c       C:\workspace\wolfssl-gojimmypi\wolfcrypt\src\port\Espressif\esp32_sha.c
+copy C:\SysGCC\esp32-8.4\esp-idf\v4.4.1\components\wolfssl\wolfcrypt\src\port\Espressif\esp32_mp.c        C:\workspace\wolfssl-gojimmypi\wolfcrypt\src\port\Espressif\esp32_mp.c
+copy C:\SysGCC\esp32-8.4\esp-idf\v4.4.1\components\wolfssl\wolfcrypt\src\port\Espressif\esp32_aes.c       C:\workspace\wolfssl-gojimmypi\wolfcrypt\src\port\Espressif\esp32_aes.c
 copy C:\SysGCC\esp32-8.4\esp-idf\v4.4.1\components\wolfssl\wolfssl\wolfcrypt\port\Espressif\esp32-crypt.h C:\workspace\wolfssl-gojimmypi\wolfssl\wolfcrypt\port\Espressif\esp32-crypt.h
