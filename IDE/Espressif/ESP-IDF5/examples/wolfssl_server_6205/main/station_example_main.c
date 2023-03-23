61ec21a63e0b6618d8300bc813a701c9b2759679
gojimmypi <jim@wolfssl.com>
2023-03-22 17:27:51 -0700

    add signed CSR

--- IDE/Espressif/ESP-IDF5/examples/wolfssl_server_6205/main/station_example_main.c
+++ IDE/Espressif/ESP-IDF5/examples/wolfssl_server_6205/main/station_example_main.c
@@ -19,7 +19,27 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
+/*
+** This ESP32 project does not use the generated options.h
+#include <wolfssl/options.h>
+*/
+#include <wolfssl/wolfcrypt/settings.h>
+
+/* properly configured, user_settings.h should have been captured in above
+** settings.h but is included here for reference: */
 #include "user_settings.h"
+
+#include <wolfssl/wolfcrypt/ecc.h>
+#include <wolfssl/wolfcrypt/rsa.h>
+#include <wolfssl/wolfcrypt/ed25519.h>
+#include <wolfssl/wolfcrypt/asn_public.h>
+#include <wolfssl/wolfcrypt/error-crypt.h>
+
+#include <wolfssl/wolfcrypt/random.h>
+#include <wolfssl/wolfcrypt/rsa.h>
+#include <wolfssl/wolfcrypt/asn.h>
+
+
 
 /* The examples use WiFi configuration that you can set via project configuration menu
 
@@ -71,6 +91,7 @@ static EventGroupHandle_t s_wifi_event_group;
 #define WIFI_FAIL_BIT      BIT1
 
 static const char *TAG = "wifi station";
+#define WOLFSSL_VERSION_PRINTF(...) ESP_LOGI(TAG, __VA_ARGS__)
 
 static int s_retry_num = 0;
 
@@ -153,8 +174,8 @@ void wifi_init_sta(void)
     /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
      * happened. */
     if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+//        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+//                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
     } else if (bits & WIFI_FAIL_BIT) {
         ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
                  EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
@@ -163,14 +184,33 @@ void wifi_init_sta(void)
     }
 }
 
-#include <wolfssl/wolfcrypt/random.h>
-#include <wolfssl/wolfcrypt/rsa.h>
-#include <wolfssl/wolfcrypt/asn.h>
-
 void app_main(void)
 {
-    //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
+    #define DER_SIZE 4096
+    #define PEM_SIZE 4096
+    #define KEY_SIZE 512
+
+    byte der[DER_SIZE];
+    byte pem[PEM_SIZE];
+    esp_err_t ret; /* return codes */
+    int derSz = 0; /* DER size found */
+
+    Cert request;
+    RsaKey genKey;
+    RNG rng;
+
+    XMEMSET(der, 0, DER_SIZE);
+
+#if !defined(WOLFSSL_CERT_REQ) || !defined(WOLFSSL_CERT_GEN) || !defined(WOLFSSL_KEY_GEN)
+    ESP_LOGI(TAG,
+             "ERROR: Need to enable "
+             "WOLFSSL_CERT_REQ, and/or "
+             "WOLFSSL_CERT_GEN, and/or "
+             "WOLFSSL_KEY_GEN");
+#endif
+
+    /* Initialize non-volatile storage */
+    ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
       ESP_ERROR_CHECK(nvs_flash_erase());
       ret = nvs_flash_init();
@@ -178,39 +218,193 @@ void app_main(void)
     ESP_ERROR_CHECK(ret);
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    /* Not only are certs needed for secure connections, but secure certs
+    ** need random numbers; Espressif RNG uses WiFi for entropy.
+    **
+    ** see https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/random.html
+    **
+    ** Otherwise consider using bootloader_random_enable().
+    */
     wifi_init_sta();
-
-    RsaKey genKey;
-    RNG    rng;
-
     wc_InitRng(&rng);
+
+    /*
+    ** ensure Digital Signature (DS) is not active
+    ** Exit the operation: Write 1 to DS_SET_FINISH_REG, then poll DS_QUERY_BUSY_REG until it reads 0
+    **
+    DPORT_REG_WRITE(DS_SET_FINISH_REG,1);
+    while (DPORT_REG_READ(DS_QUERY_BUSY_REG) != 0)
+    {
+        ESP_LOGI(TAG, "Waiting for the DS_QUERY_BUSY_REG to complete.");
+    }
+    periph_module_disable(PERIPH_DS_MODULE);
+    asm volatile("memw");
+    */
+
+    /*
+    ***************************************************************************
+    ** Create our RSA private key for the cert.
+    ***************************************************************************
+    */
     wc_InitRsaKey(&genKey, 0);
 
-    ret = wc_MakeRsaKey(&genKey, 1024, 65537, &rng);
+    ESP_LOGI(TAG, "Generating an RSA key %d bits long", KEY_SIZE);
+    ret = wc_MakeRsaKey(&genKey, KEY_SIZE, 65537, &rng);
     if (ret != 0) {
         ESP_LOGI(TAG, "wc_MakeRsaKey error %d", ret);
     }
 
-    byte der[4096];
-    int  derSz = wc_RsaKeyToDer(&genKey, der, sizeof(der));
+
+    derSz = wc_RsaKeyToDer(&genKey, der, sizeof(der));
     if (derSz < 0) {
-        ESP_LOGI(TAG, "wc_RsaKeyToDer error %d", ret);
+        ESP_LOGE(TAG, "wc_RsaKeyToDer error %d", ret);
     }
 
-    byte pem[4096];
-    int  pemSz = wc_DerToPem(der, derSz, pem, sizeof(pem),
-                      PRIVATEKEY_TYPE);
+    int  pemSz = wc_DerToPem(der, derSz,
+                             pem, sizeof(pem),
+                             PRIVATEKEY_TYPE);
     if (pemSz < 0) {
-        ESP_LOGI(TAG, "wc_DerToPem error %d", ret);
-        /* pemSz contains error */
-        ;
+        ESP_LOGE(TAG, "wc_DerToPem error %d", ret);
+    }
+    else {
+        if (pemSz < PEM_SIZE) {
+            pem[pemSz] = 0x0; /* We'll print this, so add string terminator. */
+        }
     }
 
-    Cert request;
+    ESP_LOGI(TAG, "\n\n"
+                   "Cert pem:"
+                   "\n\n"
+                   "%s"
+                   "\n\n"
+                   , pem
+            );
+
+
+    /*
+    ***************************************************************************
+    ** Create Request
+    ***************************************************************************
+    */
     wc_InitCert(&request);
 
-    // ret = wc_MakeCertReq(&request, der, sizeof(der), NULL, &key);
+    strncpy(request.subject.country, "NZ", CTC_NAME_SIZE);
+    strncpy(request.subject.state, "Waikato", CTC_NAME_SIZE);
+    strncpy(request.subject.locality, "Hamilton", CTC_NAME_SIZE);
+    strncpy(request.subject.org, "Blue Leaf Software, Ltd", CTC_NAME_SIZE);
+
+#if defined(NO_ESP32WROOM32_CRYPT)
+    strncpy(request.subject.commonName, "Lumos SW, serialNumber = BLS-001", CTC_NAME_SIZE);
+#else
+    strncpy(request.subject.commonName, "Lumos SW, serialNumber = BLS-001", CTC_NAME_SIZE);
+#endif
+
+    request.version = 0;
+    ret = wc_MakeCertReq_ex(&request, der, sizeof(der), RSA_TYPE, &genKey);
+    if (ret <= 0) {
+        ESP_LOGE(TAG, "Make Cert Req failed: %d\n", ret);
+    }
+    derSz = ret;
 
 
-    ESP_LOGI(TAG, "%s", pem);
+    /*
+    ***************************************************************************
+    ** Sign our cert request with the key generated (see above)
+    ***************************************************************************
+    */
+    request.sigType = CTC_SHA256wRSA;
+    ret = wc_SignCert_ex(request.bodySz, request.sigType,
+                         der, sizeof(der),
+                         RSA_TYPE,
+                         &genKey, &rng
+                        );
+    if (ret <= 0) {
+        ESP_LOGE(TAG, "Sign Cert failed: %d\n", ret);
+    }
+    derSz = ret;
+
+    /* convert the DER to PEM */
+    pemSz = wc_DerToPem(der, derSz,
+                        pem, sizeof(pem),
+                        CERTREQ_TYPE
+                       );
+    if (pemSz < 0) {
+        ESP_LOGE(TAG, "wc_DerToPem error %d", ret);
+    }
+    else {
+        if (pemSz < PEM_SIZE) {
+            pem[pemSz] = 0x0;
+        }
+    }
+
+    /*
+    ***************************************************************************
+    ** Show Certificate Request
+    **
+    ** Paste the following "Request pem:" into an output file.
+    **
+    ** To validate:
+    **
+    ** openssl req -in "./output/gojimmypi-ESP32_request.crt" -noout -text
+    **
+    ***************************************************************************
+    */
+    ESP_LOGI(TAG, "\n\n");
+    ESP_LOGI(TAG, "\n\n"
+                   "Request pem: (paste into file for CA sign)"
+                   "\n\n"
+                   "%s"
+                   "\n\n"
+                   , pem
+            );
+
+    /*
+    ***************************************************************************
+    ** Sign the Certificate Request with OpenSSL
+    ***************************************************************************
+
+openssl x509 -req -in "./output/gojimmypi-ESP32_request.crt" -days 10     \
+    -CA "private/CertificateAuthority-4096-PublicKey.crt"                 \
+    -CAkey "private/CertificateAuthority-4096-PrivateKey.pem"             \
+    -passin pass:secret -CAcreateserial                                   \
+    -extfile "private/CertificateExtensions.txt" -extensions LumosDevice  \
+    -out ./output/gojimmypi-Device-Certificate_hw.crt -text
+
+    */
+    ESP_LOGI(TAG, "Done!");
+
+    /* check to see if we are using hardware encryption */
+#if defined(NO_ESP32WROOM32_CRYPT)
+    WOLFSSL_VERSION_PRINTF("NO_ESP32WROOM32_CRYPT defined! "
+                           "HW acceleration DISABLED.");
+#else
+    /* first show what platform hardware acceleration is enabled
+    ** (some new platforms may not be supported yet) */
+    #if defined(CONFIG_IDF_TARGET_ESP32)
+        WOLFSSL_VERSION_PRINTF("ESP32WROOM32_CRYPT is enabled for ESP32.");
+    #elif defined(CONFIG_IDF_TARGET_ESP32S2)
+        WOLFSSL_VERSION_PRINTF("ESP32WROOM32_CRYPT is enabled for ESP32-S2.");
+    #elif defined(CONFIG_IDF_TARGET_ESP32S3)
+        WOLFSSL_VERSION_PRINTF("ESP32WROOM32_CRYPT is enabled for ESP32-S3.");
+    #else
+        #error "ESP32WROOM32_CRYPT not yet supported on this IDF TARGET"
+    #endif
+
+    /* Even though enabled, some specifics may be disabled */
+    #if defined(NO_WOLFSSL_ESP32WROOM32_CRYPT_HASH)
+        WOLFSSL_VERSION_PRINTF("NO_WOLFSSL_ESP32WROOM32_CRYPT_HASH is defined!"
+                               "(disabled HW SHA).");
+    #endif
+
+    #if defined(NO_WOLFSSL_ESP32WROOM32_CRYPT_AES)
+        WOLFSSL_VERSION_PRINTF("NO_WOLFSSL_ESP32WROOM32_CRYPT_AES is defined!"
+                               "(disabled HW AES).");
+    #endif
+
+    #if defined(NO_WOLFSSL_ESP32WROOM32_CRYPT_RSA_PRI)
+        WOLFSSL_VERSION_PRINTF("NO_WOLFSSL_ESP32WROOM32_CRYPT_RSA_PRI defined!"
+                               "(disabled HW RSA)");
+    #endif
+#endif
+
 }
