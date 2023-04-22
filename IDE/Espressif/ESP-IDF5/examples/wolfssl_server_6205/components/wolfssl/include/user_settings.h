3d02a53ed7e65750e22cb912779e636732753a11
gojimmypi <jim@wolfssl.com>
2023-04-22 10:11:32 +0200

    2048 bit RSA success; no optimization debug

--- IDE/Espressif/ESP-IDF5/examples/wolfssl_server_6205/components/wolfssl/include/user_settings.h
+++ IDE/Espressif/ESP-IDF5/examples/wolfssl_server_6205/components/wolfssl/include/user_settings.h
@@ -140,19 +140,19 @@
 //#define NO_ESP32WROOM32_CRYPT
 //#define NO_WOLFSSL_ESP32WROOM32_CRYPT_HASH
 //#define NO_WOLFSSL_ESP32WROOM32_CRYPT_AES
-// #define NO_WOLFSSL_ESP32WROOM32_CRYPT_RSA_PRI
+//#define NO_WOLFSSL_ESP32WROOM32_CRYPT_RSA_PRI
 
 /* adjust wait-timeout count if you see timeout in RSA HW acceleration */
 #define ESP_RSA_TIMEOUT_CNT    0x249F00
 
 // #define HASH_SIZE_LIMIT /* for test.c */
 
-/* test 0002  */
-// #define USE_FAST_MATH
+/* test 0003  */
+#define USE_FAST_MATH
 #define WOLFSSL_SMALL_STACK
 
 #if defined(CONFIG_IDF_TARGET_ESP32S3) && !defined(NO_WOLFSSL_ESP32WROOM32_CRYPT_AES)
-    /* AES192 is not supported on the ESP32-S3 HW at this time */
+    /* AES192 is not supported on the ESP32-S3 HW at this time TODO #pragma warn */
     #define NO_AES_192
 #endif
 
