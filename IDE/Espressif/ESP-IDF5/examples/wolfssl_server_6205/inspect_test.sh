ce6c2bdb469247b9d012e682e5347ad21ae6a83c
gojimmypi <jim@wolfssl.com>
2023-04-25 20:28:51 +0200

    make test_request.der from pem

--- IDE/Espressif/ESP-IDF5/examples/wolfssl_server_6205/inspect_test.sh
+++ IDE/Espressif/ESP-IDF5/examples/wolfssl_server_6205/inspect_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 openssl asn1parse -in "./output/test_request.crt" | tee test_request.asn.txt
 
+openssl x509 -req -outform der -in "./output/test_request.crt" -out "./output/test_request.der"
+
 openssl x509 -req -in "./output/test_request.crt" -days 10 \
                       -CA "private/CertificateAuthority-4096-PublicKey.crt" \
                       -CAkey "private/CertificateAuthority-4096-PrivateKey.pem" \
@@ -8,3 +10,5 @@ openssl x509 -req -in "./output/test_request.crt" -days 10 \
                       -extfile "private/CertificateExtensions.txt" \
                       -extensions LumosDevice \
                       -out "./output/test-Device-Certificate_hw.crt" -text
+
+openssl x509 -req -outform der -in "./output/test_request.crt" -out "./output/test_request.der"
