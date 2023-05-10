dff3ef41cd68ebb687bb67121218d1a6ffed365c
gojimmypi <jim@wolfssl.com>
2023-05-10 15:01:30 -0700

    use distro openssl, not our local build

--- IDE/Espressif/ESP-IDF5/examples/wolfssl_server_6205/inspect_test.sh
+++ IDE/Espressif/ESP-IDF5/examples/wolfssl_server_6205/inspect_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-/mnt/c/workspace/openssl/apps/openssl asn1parse -in "./output/test_request.crt" | tee test_request.asn.txt
+openssl asn1parse -in "./output/test_request.crt" | tee test_request.asn.txt
 
-/mnt/c/workspace/openssl/apps/openssl x509 -req -outform der -in "./output/test_request.crt" -out "./output/test_request.der"
+openssl x509 -req -outform der -in "./output/test_request.crt" -out "./output/test_request.der"
 
-/mnt/c/workspace/openssl/apps/openssl x509 -req -in "./output/test_request.crt" -days 10 \
+openssl x509 -req -in "./output/test_request.crt" -days 10 \
                       -CA "private/CertificateAuthority-4096-PublicKey.crt" \
                       -CAkey "private/CertificateAuthority-4096-PrivateKey.pem" \
                       -passin pass:secret -CAcreateserial \
@@ -12,4 +12,4 @@
                       -extensions LumosDevice \
                       -out "./output/test-Device-Certificate_hw.crt" -text
 
-/mnt/c/workspace/openssl/apps/openssl x509 -req -outform der -in "./output/test_request.crt" -out "./output/test_request.der"
+openssl x509 -req -outform der -in "./output/test_request.crt" -out "./output/test_request.der"
