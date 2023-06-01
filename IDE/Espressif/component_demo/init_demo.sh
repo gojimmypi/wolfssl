6ead1be7eb3813d2a30fcfcac0db2475bbd643b4
gojimmypi <jim@wolfssl.com>
2023-05-31 18:28:53 -0700

    working demo with v1.0.4-dev

--- IDE/Espressif/component_demo/init_demo.sh
+++ IDE/Espressif/component_demo/init_demo.sh
@@ -1,4 +1,4 @@
-. /mnt/c/SysGCC/esp32/esp-idf/v5.0/export.sh
+# . /mnt/c/SysGCC/esp32/esp-idf/v5.0/export.sh
 
 
 rm -rf ./build
@@ -7,6 +7,10 @@ rm ./main/idf_component.yml
 rm ./sdkconfig
 rm ./dependencies.lock
 
-idf.py add-dependency "wolfssl/wolfssl^1.0.3-dev"
+idf.py add-dependency "wolfssl/wolfssl^1.0.4-dev"
 
 idf.py build
+
+idf.py -b 115200 -p /dev/ttyS23 flash
+
+idf.py -b 115200 -p /dev/ttyS23 monitor
