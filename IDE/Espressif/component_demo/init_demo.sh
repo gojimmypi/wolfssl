c4a9b185fd3f97a0df6d11b6d1c00de5781856a1
gojimmypi <jim@wolfssl.com>
2023-05-31 17:41:50 -0700

    test 1.0.3-dev, still fails to compile, missing freertos & types

--- IDE/Espressif/component_demo/init_demo.sh
+++ IDE/Espressif/component_demo/init_demo.sh
@@ -1,5 +1,12 @@
 . /mnt/c/SysGCC/esp32/esp-idf/v5.0/export.sh
 
-idf.py add-dependency "wolfssl/wolfssl^1.0.1-dev"
+
+rm -rf ./build
+rm -rf ./managed_components/
+rm ./main/idf_component.yml
+rm ./sdkconfig
+rm ./dependencies.lock
+
+idf.py add-dependency "wolfssl/wolfssl^1.0.3-dev"
 
 idf.py build
