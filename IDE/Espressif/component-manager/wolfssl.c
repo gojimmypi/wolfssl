6c5537d26f7affa2d4d4bf77537c5f4cff50e66e
gojimmypi <jim@wolfssl.com>
2023-06-01 14:56:43 -0700

    initial Espressif/component-manager

---
+++ IDE/Espressif/component-manager/wolfssl.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "wolfssl.h"
+
+void func(void)
+{
+
+}
---
+++ IDE/Espressif/component-manager/wolfssl_component_publish.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# check if IDF_PATH is set
+if [ -z "$IDF_PATH" ]; then
+    echo "Please follows the instruction of ESP-IDF installation and set IDF_PATH."
+    exit 1
+fi
+
+# make sure it actually exists
+if [ ! -d "$IDF_PATH" ]; then
+    echo "ESP-IDF Development Framework doesn't exist.: $IDF_PATH"
+    exit 1
+fi
+
+# is export.sh in the IDF path?
+if [ ! -e "$IDF_PATH/export.sh" ]; then
+    echo "ESP-IDF export.sh: $IDF_PATH/export.sh"
+    exit 1
+fi
+
+echo ""
+echo "Publishing local wolfSSL source to ESP Registry: components.espressif.com"
+echo ""
+echo "WARNING: The live wolfSSL will be replaced upon completion."
+echo ""
+echo "Current source directory:"
+echo ""
+pwd
+echo ""
+
+COMPONENT_MANAGER_PUBLISH=
+until [ "${COMPONENT_MANAGER_PUBLISH^}" == "Y" ] || [ "${COMPONENT_MANAGER_PUBLISH^}" == "N" ]; do
+    read -n1 -p "Proceed (Y/N) " COMPONENT_MANAGER_PUBLISH
+    COMPONENT_MANAGER_PUBLISH=${COMPONENT_MANAGER_PUBLISH^};
+    echo;
+done
+
+if [ "${COMPONENT_MANAGER_PUBLISH}" == "Y" ]; then
+    echo;
+    echo "Here we go!"
+    # compote component upload --namespace wolfssl --name wolfssl
+else
+    echo;
+    echo "No files published!"
+fi
+
+
