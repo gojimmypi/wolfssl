9274457fba03add3ed4cf8620c52668b3817f42a
gojimmypi <jim@wolfssl.com>
2023-06-23 10:29:21 -0700

    WOLFSSL_BENCHMARK_FIXED_UNITS_KB & other tests

--- user_settings.h
+++ user_settings.h
@@ -2,5 +2,30 @@
 // #define WOLFSSL_SMALL_STACK
 // #define WOLFSSL_TRACK_MEMORY /* there's not #include <pthread.h> */
 // #define WOLFSSL_DEBUG_MEMORY
-// #define NO_CODING
-// #define NO_ASN
+#define WOLFSSL_BENCHMARK_FIXED_UNITS_KB
+
+// 6/21 #define BENCH_EMBEDDED
+
+// 6/21     #define USE_CERT_BUFFERS_2048
+
+// #define WOLFSSL_TLS13
+// #define HAVE_TLS_EXTENSIONS
+//#define WC_RSA_PSS
+//#define HAVE_HKDF
+
+// 6/21 #define HAVE_AEAD
+
+//#define HAVE_SUPPORTED_CURVES
+
+/* when you want to use SINGLE THREAD */
+/* #define SINGLE_THREADED */
+// #define NO_FILESYSTEM
+
+// #define HAVE_AESGCM
+/* when you want to use SHA384 */
+/* #define WOLFSSL_SHA384 */
+//#define WOLFSSL_SHA512
+//#define HAVE_ECC
+//#define HAVE_CURVE25519
+//#define CURVE25519_SMALL
+// #define HAVE_ED25519
