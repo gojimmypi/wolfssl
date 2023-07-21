2c4f443aec7b151f945cb9dfe2dad6ee30449cf0
gojimmypi <jim@wolfssl.com>
2023-07-21 09:56:26 -0700

    initial sm server readme

--- gencert_sm.sh
+++ gencert_sm.sh
@@ -14,9 +14,18 @@ openssl x509 -in ./certs/sm2/client-sm2.pem -outform der -out ./my-client-sm2.de
 openssl ec -in ./certs/sm2/client-sm2-priv.pem -outform der -out ./my-client-sm2-priv.der
 
 ./scripts/dertoc.pl ./certs/sm2/client-sm2.pem      client_sm2      ./my_client_sm2.c
-./scripts/dertoc.pl ./my-client-sm2-priv.der client_sm2_priv ./my_client_sm2_priv.c
+./scripts/dertoc.pl ./my-client-sm2-priv.der        client_sm2_priv ./my_client_sm2_priv.c
 
 ./scripts/dertoc.pl ./certs/sm2/client-sm2-priv.pem client_sm2_priv ./my_client_sm2_priv.c
+
+
+./scripts/dertoc.pl ./certs/sm2/ca-sm2.der          ca_sm2      ./my_ca_sm2.c
+
+./scripts/dertoc.pl  ./certs/sm2/server-sm2.pem       server_sm2      ./my_server-sm2_pem.c
+./scripts/dertoc.pl  ./certs/sm2/server-sm2-priv.pem  server_sm2_priv ./my_server_sm2_priv_pem.c
+./scripts/dertoc.pl  ./certs/sm2/client-sm2.pem       client_sm2      ./my_client_sm2_pem.c
+
+./certs/sm2/root-sm2.pem
 
 # ./examples/client/client -v 4 -l TLS13-SM4-GCM-SM3 \
 #     -c ./certs/sm2/client-sm2.pem -k ./certs/sm2/client-sm2-priv.pem \
