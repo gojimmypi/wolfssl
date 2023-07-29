0b540dcf53ce6c091f2976e14c3371102b17b62f
gojimmypi <jim@wolfssl.com>
2023-07-29 09:39:26 -0700

    Paul's TryAquireHardware

--- wolfcrypt/src/port/Espressif/esp32_sha.c
+++ wolfcrypt/src/port/Espressif/esp32_sha.c
@@ -60,16 +60,17 @@ static const char* TAG = "wolf_hw_sha";
     #define WC_SHA_DIGEST_SIZE 20
 #endif
 
-#define DEBUG_WOLFSSL_SHA_MUTEX
+#define DEBUG_WOLFSSL_SHA_MUTEX /* TODO */
 /* RTOS mutex or just InUse variable  */
-#if defined(SINGLE_THREADED)
-    static int InUse = 0;
-#else
-    static wolfSSL_Mutex sha_mutex = NULL;
+
+static int InUse = 0;
+/* If not single threaded, protect InUse with a critical section */
+#if !defined(SINGLE_THREADED) /* the wolfSSL setting */
+    static portMUX_TYPE sha_crit_sect = portMUX_INITIALIZER_UNLOCKED;
 #endif
 
 #ifdef DEBUG_WOLFSSL_SHA_MUTEX
-    static int mutex_ctx_owner = NULL;
+    static int mutex_ctx_owner = 0;
 #endif
 
 #if defined(DEBUG_WOLFSSL)
@@ -650,6 +651,7 @@ int esp_unroll_sha_module_enable(WC_ESP32SHA* ctx)
             ** periph_module_disable() or threading not working properly.
             **/
             ESP_LOGW(TAG, "warning lockDepth mismatch.");
+          printf("Bad lock depth @ %d = %d for WC_ESP32SHA @ %0xd\n", __LINE__, ctx->lockDepth, (unsigned)ctx);
         }
         ctx->lockDepth = 0;
         ctx->mode = ESP32_SHA_INIT;
@@ -666,10 +668,38 @@ int esp_unroll_sha_module_enable(WC_ESP32SHA* ctx)
     return ret;
 } /* esp_unroll_sha_module_enable */
 
-
 /*
-**
+** lock HW engine.
+** this should be called before using engine.
 */
+bool TryAcquireHardware()
+{
+    bool bCanUseHardware = false;
+    taskENTER_CRITICAL(&sha_crit_sect);
+        if (!InUse)
+        {
+            InUse = 1;
+            bCanUseHardware = true;
+        }
+    taskEXIT_CRITICAL(&sha_crit_sect);
+
+    return bCanUseHardware;
+}
+
+void ReleaseHardware()
+{
+    taskENTER_CRITICAL(&sha_crit_sect);
+        if (InUse)
+        {
+            InUse = 0;
+        }
+        else
+        {
+            assert(false); // bug: releasing hardware when not in use!!
+        }
+    taskEXIT_CRITICAL(&sha_crit_sect);
+}
+
 int esp_sha_hw_islocked(WC_ESP32SHA* ctx)
 {
     int ret = 0;
@@ -742,32 +772,6 @@ int esp_sha_try_hw_lock(WC_ESP32SHA* ctx)
     **
     */
 
-    if (sha_mutex == NULL) {
-        ESP_LOGV(TAG, "Initializing sha_mutex");
-
-        /* created, but not yet locked */
-        ret = esp_CryptHwMutexInit(&sha_mutex);
-        if (ret == 0) {
-            ESP_LOGI(TAG, "esp_CryptHwMutexInit sha_mutex init success.");
-        #ifdef DEBUG_WOLFSSL_SHA_MUTEX
-            mutex_ctx_owner = 0;
-        #endif
-        }
-        else {
-            ESP_LOGE(TAG, "esp_CryptHwMutexInit sha_mutex failed.");
-            sha_mutex = 0;
-
-            ESP_LOGI(TAG, "Revert to ctx->mode = ESP32_SHA_SW.");
-
-        #ifdef DEBUG_WOLFSSL_SHA_MUTEX
-            ESP_LOGI(TAG, "Current mutext owner = %x", mutex_ctx_owner);
-        #endif
-
-            ctx->mode = ESP32_SHA_SW;
-            return 0; /* success, just not using HW */
-        }
-    }
-
     /* check if this SHA has been operated as SW or HW, or not yet init */
     if (ctx->mode == ESP32_SHA_INIT) {
         /* try to lock the HW engine */
@@ -776,7 +780,9 @@ int esp_sha_try_hw_lock(WC_ESP32SHA* ctx)
         /* we don't wait:
         ** either the engine is free, or we fall back to SW
         **/
-        if (esp_CryptHwMutexLock(&sha_mutex, (TickType_t)0) == 0) {
+        if (TryAcquireHardware()) {
+            ctx->mode = ESP32_SHA_HW; // present in single threaded, but not here. not sure why?
+
             /* check to see if we had a prior fail and need to unroll enables */
             ret = esp_unroll_sha_module_enable(ctx);
             ESP_LOGI(TAG, "Hardware Mode Active, lock depth = %d, for %x",
@@ -791,7 +797,8 @@ int esp_sha_try_hw_lock(WC_ESP32SHA* ctx)
                 ESP_LOGW(TAG, "WARNING: Hardware Mode "
                               "interesting lock depth = %d, for %x",
                               ctx->lockDepth, (int)ctx->initializer);
-            }
+           printf("Bad lock depth @ %d: %d for WC_ESP32SHA @ %0xd\n", __LINE__, ctx->lockDepth, (unsigned)ctx);
+           }
         }
         else {
             /* We should have otherwise anticipated this; how did we get here?
@@ -818,6 +825,7 @@ int esp_sha_try_hw_lock(WC_ESP32SHA* ctx)
 #else
     if (ret == 0) {
         ctx->lockDepth++; /* depth for THIS ctx (there could be others!) */
+          printf("Lock depth @ %d = %d for WC_ESP32SHA @ %0xd\n", __LINE__, ctx->lockDepth, (unsigned)ctx);
         periph_module_enable(PERIPH_SHA_MODULE);
         ctx->mode = ESP32_SHA_HW;
     }
@@ -842,6 +850,8 @@ int esp_sha_hw_unlock(WC_ESP32SHA* ctx)
     /* ESP32-C3 RISC-V TODO */
 #else
     /* Disable AES hardware */
+    // Note: Jim, is there any cost associated with enable/disable hardware here? This seems like
+    // a power saving feature that could be handled at a different level than per-calculation.
     periph_module_disable(PERIPH_SHA_MODULE);
 #endif
     /* we'll keep track of our lock depth.
@@ -849,24 +859,25 @@ int esp_sha_hw_unlock(WC_ESP32SHA* ctx)
      * and periph_module_disable() need to be unwound.
      *
      * see ref_counts[periph] in file: periph_ctrl.c */
+          printf("Lock depth @ %d = %d for WC_ESP32SHA @ %0xd\n", __LINE__, ctx->lockDepth, (unsigned)ctx);
     if (ctx->lockDepth > 0) {
         ctx->lockDepth--;
     }
     else {
         ctx->lockDepth = 0;
     }
+          printf("Lock depth @ %d = %d for WC_ESP32SHA @ %0xd\n", __LINE__, ctx->lockDepth, (unsigned)ctx);
 
+    if (0 == ctx->lockDepth) // should we only unlock if depth is 0?
+    {
 #if defined(SINGLE_THREADED)
-    InUse = 0;
+        InUse = 0;
 #else
-    /* unlock HW engine for next use */
-    esp_CryptHwMutexUnLock(&sha_mutex);
+        /* unlock HW engine for next use */
+        ReleaseHardware();
 #endif
-
-#ifdef DEBUG_WOLFSSL_SHA_MUTEX
-    mutex_ctx_owner = 0;
-#endif
-    ESP_LOGI(TAG, "leave esp_sha_hw_unlock, %x", (int)ctx->initializer);
+    }
+    ESP_LOGV(TAG, "leave esp_sha_hw_unlock, %x", (int)ctx->initializer);
     return 0;
 } /* esp_sha_hw_unlock */
 
