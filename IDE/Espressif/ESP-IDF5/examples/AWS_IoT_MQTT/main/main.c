e9b130102f6a25b27b8ea44d933544b8e25de510
gojimmypi <jim@wolfssl.com>
2023-08-18 11:57:13 -0700

    AWS IoT Example: add wifi & time

--- IDE/Espressif/ESP-IDF5/examples/AWS_IoT_MQTT/main/main.c
+++ IDE/Espressif/ESP-IDF5/examples/AWS_IoT_MQTT/main/main.c
@@ -21,6 +21,7 @@
 
 /* Espressif */
 #include <esp_log.h>
+#include <nvs_flash.h>
 
 /* wolfSSL  */
 #include <wolfssl/wolfcrypt/port/Espressif/esp32-crypt.h>
@@ -28,20 +29,55 @@
 /* project */
 #include "main.h"
 #include "awsiot.h"
+#include "wifi_connect.h"
+#include "time_helper.h"
 
 static const char* const TAG = "My Project";
 
 void app_main(void)
 {
+    ESP_LOGI(TAG, "------------- wolfSSL wolfMQTT AWS IoT Example ---------");
+    ESP_LOGI(TAG, "--------------------------------------------------------");
+    ESP_LOGI(TAG, "--------------------------------------------------------");
+    ESP_LOGI(TAG, "---------------------- BEGIN MAIN ----------------------");
+    ESP_LOGI(TAG, "--------------------------------------------------------");
+    ESP_LOGI(TAG, "--------------------------------------------------------");
+#ifdef HAVE_VERSION_EXTENDED_INFO
+    esp_ShowExtendedSystemInfo();
+#endif
+
     ESP_LOGI(TAG, "Hello wolfSSL!");
 
 #ifdef HAVE_VERSION_EXTENDED_INFO
     esp_ShowExtendedSystemInfo();
 #endif
 
+    /* Initialize NVS */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+        ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    /* Initialize WiFi */
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    ret = wifi_init_sta();
+    if (ret != 0) {
+        ESP_LOGV(TAG, "\n\nFailed to connect to WiFi. Halt.\n\n");
+#if defined(SINGLE_THREADED)
+        // yield;
+        while (1);
+#else
+        vTaskDelay(60000);
+#endif
+    }
+
+    /* set time for cert validation */
+    set_time();
+
+    awsiot_main((int)NULL, (char**)NULL);
     ESP_LOGI(TAG, "\n\nDone!"
                   "If running from idf.py monitor, press twice: Ctrl+]");
-
-    #define NO_MAIN_DRIVER
-    awsiot_main((int)NULL, (char**)NULL);
 }
