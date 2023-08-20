05e16cb16aae75166e3c7c0da848b005d0a5d0be
gojimmypi <jim@wolfssl.com>
2023-08-20 14:29:02 -0700

    remove unused files

--- IDE/Espressif/ESP-IDF5/examples/AWS_IoT_MQTT/components/wolfmqtt/wolfmqtt/wolfMQTT.h
+++
@@ -1,6 +0,0 @@
-/* Generated wolfMQTT header file for Arduino */
-#include <wolfmqtt/mqtt_client.h>
-#ifndef WOLFSSL_USER_SETTINGS
-    #include <wolfssl/options.h>
-#endif
-#include <wolfssl/wolfcrypt/settings.h>
