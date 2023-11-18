ba9ddc7c77c33e09ab51b3135077c1c0a8780d64
gojimmypi <jim@wolfssl.com>
2023-11-17 16:15:46 -0800

    gate esp_hw_show_metrics with WOLFSSL_HW_METRICS

--- IDE/Espressif/ESP-IDF/examples/template/main/main.c
+++ IDE/Espressif/ESP-IDF/examples/template/main/main.c
@@ -38,6 +38,10 @@ void app_main(void)
     esp_ShowExtendedSystemInfo();
 #endif
 
+#ifdef WOLFSSL_HW_METRICS
+    esp_hw_show_metrics();
+#endif
+
     ESP_LOGI(TAG, "\n\nDone!"
                   "If running from idf.py monitor, press twice: Ctrl+]\n\n"
                   "WOLFSSL_COMPLETE\n" /* exit keyword for wolfssl_monitor.py */
