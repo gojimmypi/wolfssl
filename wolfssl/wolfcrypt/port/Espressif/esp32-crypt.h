ff5a6de10ce096c6ee2296aef0aea5a437482a9d
gojimmypi <jim@wolfssl.com>
2024-02-07 12:09:28 -0800

    Update esp32-crypt for wolfSSH & SINGLE_THREADED

--- wolfssl/wolfcrypt/port/Espressif/esp32-crypt.h
+++ wolfssl/wolfcrypt/port/Espressif/esp32-crypt.h
@@ -259,6 +259,11 @@ enum {
 **   See NO_HW_MATH_TEST.
 **
 *******************************************************************************
+** WOLFSSL_FULL_WOLFSSH_SUPPORT
+**   TODO - there's a known, unresolved problem with SHA256 in wolfSSH
+**   Until fixed by a release version or this macro being define once resolved,
+**   this macro should remain undefined.
+**
 */
 #ifdef WOLFSSL_ESP32_CRYPT_DEBUG
     #undef LOG_LOCAL_LEVEL
@@ -473,7 +478,10 @@ enum {
 #endif
 
 #ifdef SINGLE_THREADED
-//    #undef ESP_MONITOR_HW_TASK_LOCK
+    #ifdef WOLFSSL_DEBUG_MUTEX
+        #undef  ESP_MONITOR_HW_TASK_LOCK
+        #define ESP_MONITOR_HW_TASK_LOCK
+    #endif
 #else
     /* Unless explicitly disabled, monitor task lock when not single thread. */
     #ifndef ESP_DISABLE_HW_TASK_LOCK
@@ -644,9 +652,9 @@ extern "C"
     #endif
         /* pointer to object the initialized HW; to track copies */
         void* initializer;
-    #ifdef ESP_MONITOR_HW_TASK_LOCK
-        TaskHandle_t task_owner;
-    #endif
+#if !defined(SINGLE_THREADED) || defined(ESP_MONITOR_HW_TASK_LOCK)
+        void* task_owner;
+#endif
 
         /* an ESP32_MODE value; typically:
         **   0 init,
@@ -893,6 +901,16 @@ extern "C"
 int esp_sha_stack_check(WC_ESP32SHA* sha);
 
 #endif /* WOLFSSL_STACK_CHECK */
+
+/* Compatibility checks */
+#if defined(DEBUG_WOLFSSH) || defined(ESP_ENABLE_WOLFSSH) || \
+    defined(WOLFSSH_TERM)  || defined(WOLFSSH_TEST_SERVER)
+    #ifndef NO_WOLFSSL_ESP32_CRYPT_HASH_SHA256
+        /* need to add this line to wolfssl component user_settings.h
+         * #define NO_WOLFSSL_ESP32_CRYPT_HASH_SHA256 */
+        #error "ESP32_CRYPT_HASH_SHA256 not supported on wolfSSL at this time"
+    #endif
+#endif /* SSH SHA256 HW check */
 
 #endif /* WOLFSSL_ESPIDF (entire contents excluded when not Espressif ESP-IDF) */
 
