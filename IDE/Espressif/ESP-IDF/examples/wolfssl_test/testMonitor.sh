9bbc6e9f1b92aedc67c2c76290576a894558944f
gojimmypi <jim@wolfssl.com>
2024-03-06 10:18:56 -0800

    testMonitor can now optionally spawn a putty process

--- IDE/Espressif/ESP-IDF/examples/wolfssl_test/testMonitor.sh
+++ IDE/Espressif/ESP-IDF/examples/wolfssl_test/testMonitor.sh
@@ -5,8 +5,10 @@
 #
 # Example:
 #
-#   ./testMonitor.sh wolfssl_benchmark esp32c6 HW
+#   ./testMonitor.sh wolfssl_test esp32c6 WIP
 #
+# Define ESPIDF_PUTTY_MONITOR to a non-blank value to call putty
+# instead of using `idf.py monitor`
 #========================================================================================
 
 # Run shell check to ensure this a good script.
@@ -29,13 +31,13 @@ else
     THIS_KEYWORD="$3"
 fi
 
-
+echo "testMonitor current path:"
 pwd
 
 #ESP32c2 monitor is 78800
 
 esp32_PORT="/dev/ttyS9"
-esp32c2_PORT="/dev/ttyS35"
+esp32c2_PORT="/dev/ttyS71"
 esp32c3_PORT="/dev/ttyS35"
 esp32c6_PORT="/dev/ttyS36"
 esp32h2_PORT="/dev/ttyS31"
@@ -45,6 +47,13 @@ esp32s3_PORT="/dev/ttyS24"
 esp8684_PORT="/dev/ttyS49"
 # esp32c2_PORT="/dev/ttyS49" #8684
 
+esp32_PUTTY="COM9"
+esp32c2_PUTTY="COM71 - ESP32-C2 74880"
+esp32c3_PUTTY="COM35"
+esp32c6_PUTTY="COM36"
+esp32h2_PUTTY="COM31"
+esp32s2_PUTTY="COM30"
+esp32s3_PUTTY="COM24"
 
 echo "esp32_PORT:   $esp32_PORT"
 echo "esp32c2_PORT: $esp32c2_PORT"
@@ -57,27 +66,59 @@ echo "esp8684_PORT: $esp8684_PORT"
 
 # given a THIS_TARGET, assign THIS_TARGET_PORT to the respective port.
 THIS_TARGET_PORT="${THIS_TARGET}_PORT"
-# Check that WOLFSSL_ROOT is not defined.
+
+# Check that THIS_TARGET_PORT is defined.
 if [ -z "$THIS_TARGET_PORT" ]; then
     echo "Error: No port defined for ${THIS_TARGET}"
     exit 1
 else
     echo "THIS_TARGET_PORT=${THIS_TARGET_PORT}"
 fi
-THIS_TARGET_PORT="${!THIS_TARGET_PORT}"
 
+THIS_TARGET_PORT="${!THIS_TARGET_PORT}"
 echo THIS_TARGET_PORT="${THIS_TARGET_PORT}"
+
+
+# The use of putty is optional
+THIS_TARGET_PUTTY="${THIS_TARGET}_PUTTY"
+
+if [ -z "$ESPIDF_PUTTY_MONITOR" ]; then
+    echo "Using ESP-IDF monitor"
+else
+    # Check that THIS_TARGET_PUTTY is defined.
+    echo "esp32_PUTTY:   $esp32_PUTTY"
+    echo "esp32c2_PUTTY: $esp32c2_PUTTY"
+    echo "esp32c3_PUTTY: $esp32c3_PUTTY"
+    echo "esp32c6_PUTTY: $esp32c6_PUTTY"
+    echo "esp32s2_PUTTY: $esp32s2_PUTTY"
+    echo "esp32s3_PUTTY: $esp32s3_PUTTY"
+    echo "esp32h2_PUTTY: $esp32h2_PUTTY"
+    echo "esp8684_PUTTY: $esp8684_PUTTY"
+    echo ""
+
+    if [ -z "$THIS_TARGET_PUTTY" ]; then
+        echo "Error: No putty profile defined for ${THIS_TARGET}"
+        exit 1
+    else
+        echo "THIS_TARGET_PUTTY=${THIS_TARGET_PUTTY}"
+    fi
+
+    THIS_TARGET_PUTTY="${!THIS_TARGET_PUTTY}"
+    echo THIS_TARGET_PUTTY="${THIS_TARGET_PUTTY}"
+fi
 
 if [[ "$THIS_TARGET" == "esp8684" ]]; then
     THIS_TARGET=esp32c2
 fi
 
+
+
 # Assemble some log file names.
 echo ""
-BUILD_LOG="${THIS_HOME_DIR}/logs/${THIS_EXAMPLE}_build_IDF_v5.1_blog_${THIS_TARGET}_${THIS_KEYWORD}.txt"
-FLASH_LOG="${THIS_HOME_DIR}/logs/${THIS_EXAMPLE}_flash_IDF_v5.1_blog_${THIS_TARGET}_${THIS_KEYWORD}.txt"
-THIS_LOG="${THIS_HOME_DIR}/logs/${THIS_EXAMPLE}_output_IDF_v5.1_blog_${THIS_TARGET}_${THIS_KEYWORD}.txt"
-THIS_CFG="${THIS_HOME_DIR}/logs/${THIS_EXAMPLE}_user_settings_IDF_v5.1_blog_${THIS_TARGET}_${THIS_KEYWORD}.txt"
+BUILD_LOG="${THIS_HOME_DIR}/logs/${THIS_EXAMPLE}_build_IDF_v5.1_${THIS_TARGET}_${THIS_KEYWORD}.txt"
+FLASH_LOG="${THIS_HOME_DIR}/logs/${THIS_EXAMPLE}_flash_IDF_v5.1_${THIS_TARGET}_${THIS_KEYWORD}.txt"
+THIS_LOG="${THIS_HOME_DIR}/logs/${THIS_EXAMPLE}_output_IDF_v5.1_${THIS_TARGET}_${THIS_KEYWORD}.txt"
+THIS_CFG="${THIS_HOME_DIR}/logs/${THIS_EXAMPLE}_user_settings_IDF_v5.1_${THIS_TARGET}_${THIS_KEYWORD}.txt"
 
 # cp ./components/wolfssl/include/user_settings.h "${THIS_CFG}"
 
@@ -95,8 +136,8 @@ idf.py fullclean                                >> "${BUILD_LOG}" 2>&1
 THIS_ERROR_CODE=$?
 if [ $THIS_ERROR_CODE -ne 0 ]; then
     echo ""
-    echo "Error during fullclean"
-    exit 1
+    echo "Error during fullclean. Deleting build directory."
+    rm -rf ./build
 fi
 
 #---------------------------------------------------------------------
@@ -132,7 +173,14 @@ fi
 # popd || exit 1
 # popd || exit 1
 
-echo "Monitor..."
-echo  ./wolfssl_monitor.py --port "${THIS_TARGET_PORT}" --baudrate 115200 --logfile "${THIS_LOG}"
+# Note both of the options spawn a separate process:
+if [ -z "$ESPIDF_PUTTY_MONITOR" ]; then
+    echo "Monitor..."
+    echo  ./wolfssl_monitor.py --port "${THIS_TARGET_PORT}" --baudrate 115200 --logfile "${THIS_LOG}"
 
-./wolfssl_monitor.py --port "${THIS_TARGET_PORT}" --baudrate 115200 --logfile "${THIS_LOG}" &
+    ./wolfssl_monitor.py --port "${THIS_TARGET_PORT}" --baudrate 115200 --logfile "${THIS_LOG}" &
+else
+    echo "Calling putty..."
+    echo "/mnt/c/tools/putty.exe -load \"$THIS_TARGET_PUTTY\""
+    /mnt/c/tools/putty.exe -load "$THIS_TARGET_PUTTY" &
+fi
