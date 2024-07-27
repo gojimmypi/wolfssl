9bdf7c4ffd99a8b8e2feb7eba5e04103556d3108
gojimmypi <jim@wolfssl.com>
2024-07-27 12:39:38 -0700

    remove extra esp_crt_verify_callback_X

--- wolfcrypt/src/port/Espressif/esp_crt_bundle-wolfssl.c
+++ wolfcrypt/src/port/Espressif/esp_crt_bundle-wolfssl.c
@@ -80,85 +80,13 @@ void wolfssl_ssl_conf_verify(wolfssl_ssl_config *conf,
 }
 //#endif /* WOLFSSL_X509_CRT_PARSE_C */
 
-int esp_crt_verify_callback(void *buf, WOLFSSL_X509 *crt, int depth, uint32_t *flags)
-{
-    int *child = crt;
-
-    /* It's OK for a trusted cert to have a weak signature hash alg.
-       as we already trust this certificate */
-//    uint32_t flags_filtered = *flags & ~(WOLFSSL_X509_BADCERT_BAD_MD);
-//
-//    if (flags_filtered != WOLFSSL_X509_BADCERT_NOT_TRUSTED) {
-//        return 0;
-//    }
-
-
-    if (s_crt_bundle.crts == NULL) {
-        ESP_LOGE(TAG, "No certificates in bundle");
-        return ESP_FAIL;
-    }
-
-    ESP_LOGD(TAG, "%d certificates in bundle", s_crt_bundle.num_certs);
-
-    size_t name_len = 0;
-    const uint8_t *crt_name;
-
-    bool crt_found = false;
-    int start = 0;
-    int end = s_crt_bundle.num_certs - 1;
-    int middle = (end - start) / 2;
-
-    /* Look for the certificate using binary search on subject name */
-    while (start <= end) {
-        name_len = s_crt_bundle.crts[middle][0] << 8 | s_crt_bundle.crts[middle][1];
-        crt_name = s_crt_bundle.crts[middle] + CRT_HEADER_OFFSET;
-
-        int cmp_res = 0; // memcmp(child->issuer_raw.p, crt_name, name_len );
-        if (cmp_res == 0) {
-            crt_found = true;
-            break;
-        } else if (cmp_res < 0) {
-            end = middle - 1;
-        } else {
-            start = middle + 1;
-        }
-        middle = (start + end) / 2;
-    }
-
-    int ret = -1; /* TODO  WOLFSSL_ERR_X509_FATAL_ERROR; */
-    if (crt_found) {
-        size_t key_len = s_crt_bundle.crts[middle][2] << 8 | s_crt_bundle.crts[middle][3];
-        // ret = esp_crt_check_signature(child, s_crt_bundle.crts[middle] + CRT_HEADER_OFFSET + name_len, key_len);
-    }
-
-    if (ret == 0) {
-        ESP_LOGI(TAG, "Certificate validated");
-        *flags = 0;
-        return 0;
-    }
-
-    ESP_LOGE(TAG, "Failed to verify certificate");
-    return ESP_FAIL; /* TOSO wolfssl fail? */
-}
-void esp_crt_bundle_detach(wolfssl_ssl_config *conf)
-{
-    free(s_crt_bundle.crts);
-    s_crt_bundle.crts = NULL;
-    if (conf) {
-        wolfssl_ssl_conf_verify(conf, NULL, NULL);
-        ESP_LOGE(TAG, "esp_crt_bundle_detach not implemented for wolfSSL");
-    }
-    ESP_LOGE(TAG, "Not implemented: esp_crt_bundle_detach");
-}
-
-
 /* This callback is called for every certificate in the chain. If the chain
  * is proper each intermediate certificate is validated through its parent
  * in the x509_crt_verify_chain() function. So this callback should
  * only verify the first untrusted link in the chain is signed by the
  * root certificate in the trusted bundle
 */
-int esp_crt_verify_callback_X(void *buf, WOLFSSL_X509 *crt, int depth, uint32_t *flags)
+int esp_crt_verify_callback(void *buf, WOLFSSL_X509 *crt, int depth, uint32_t *flags)
 {
     WOLFSSL_X509 *child = crt;
 
@@ -217,7 +145,7 @@ int esp_crt_verify_callback_X(void *buf, WOLFSSL_X509 *crt, int depth, uint32_t 
 
     ESP_LOGE(TAG, "Failed to verify certificate");
     return -1; // WOLFSSL_ERR_X509_FATAL_ERROR;
-}
+} /* esp_crt_verify_callback */
 
 #else
     #warning "No TLS library selected"
@@ -307,6 +235,19 @@ esp_err_t esp_crt_bundle_attach(void *conf)
     ESP_LOGI(TAG, "esp_crt_bundle_attach completed for wolfSSL");
     return ret;
 }
+
+void esp_crt_bundle_detach(wolfssl_ssl_config *conf)
+{
+    free(s_crt_bundle.crts);
+    s_crt_bundle.crts = NULL;
+    if (conf) {
+        wolfssl_ssl_conf_verify(conf, NULL, NULL);
+        ESP_LOGE(TAG, "esp_crt_bundle_detach not implemented for wolfSSL");
+    }
+    ESP_LOGE(TAG, "Not implemented: esp_crt_bundle_detach");
+}
+
+
 
 
 esp_err_t esp_crt_bundle_set(const uint8_t *x509_bundle, size_t bundle_size)
