ef3731b964a5a57fbca6d29e3583a19663fe0fd7
gojimmypi <jim@wolfssl.com>
2024-09-05 14:57:28 -0700

    wolfssl_pio_build.bat

---
+++ IDE/Espressif/ESP-IDF/examples/esp_http_client_example/wolfssl_pio_build.bat
@@ -0,0 +1,12 @@
+::!/bin/dos
+
+:: Delete the current sdfconfig to ensure defaults are loaded
+:: rm sdkconfig.esp32dev
+
+:: Remove the ensire prior .pio build directory to ensure a clean, fresh build
+rmdir .pio /s /q
+
+:: build and upload
+pio run --target upload --upload-port COM19
+
+c:\tools\putty.exe -load COM19
