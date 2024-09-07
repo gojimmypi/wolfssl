5db4af25d0005c1d59912adc01b096f92c60495d
gojimmypi <jim@wolfssl.com>
2024-09-06 17:13:48 -0700

    step error checking & exit

--- IDE/Espressif/ESP-IDF/examples/esp_http_client_example/wolfssl_pio_build.bat
+++ IDE/Espressif/ESP-IDF/examples/esp_http_client_example/wolfssl_pio_build.bat
@@ -6,7 +6,19 @@
 :: Remove the ensire prior .pio build directory to ensure a clean, fresh build
 rmdir .pio /s /q
 
-:: build and upload
+:: build
+:: pio run
+pio run --verbose > build.log 2>&1
+if %ERRORLEVEL% neq 0 (
+    @echo An error occurred during build. Exiting with error code %ERRORLEVEL%.
+    exit /b %ERRORLEVEL%
+)
+
+:: upload and launch putty
 pio run --target upload --upload-port COM19
+if %ERRORLEVEL% neq 0 (
+    @echo An error occurred during upload. Exiting with error code %ERRORLEVEL%.
+    exit /b %ERRORLEVEL%
+)
 
 c:\tools\putty.exe -load COM19
