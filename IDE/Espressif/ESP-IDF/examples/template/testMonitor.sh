9ae70a37a696d2779b6e23437948b671ce1deb38
gojimmypi <jim@wolfssl.com>
2025-01-29 17:43:02 -0800

    sync make/cmake updates to other examples

--- IDE/Espressif/ESP-IDF/examples/template/testMonitor.sh
+++ IDE/Espressif/ESP-IDF/examples/template/testMonitor.sh
@@ -166,6 +166,7 @@ FLASH_LOG="${THIS_HOME_DIR}/logs/${THIS_EXAMPLE}_flash_IDF_${THIS_VERSION}_${THI
 THIS_LOG="${THIS_HOME_DIR}/logs/${THIS_EXAMPLE}_output_IDF_${THIS_VERSION}_${THIS_TARGET}_${THIS_KEYWORD}.txt"
 THIS_CFG="${THIS_HOME_DIR}/logs/${THIS_EXAMPLE}_user_settings_IDF_${THIS_VERSION}_${THIS_TARGET}_${THIS_KEYWORD}.txt"
 THIS_WLOG="logs\\${THIS_TARGET}_output.log"
+mkdir -p "${THIS_HOME_DIR}/logs"
 # cp ./components/wolfssl/include/user_settings.h "${THIS_CFG}"
 
 echo  "BUILD_LOG = ${BUILD_LOG}"
@@ -183,6 +184,7 @@ else
     # Get the ESP-IDF version
     # Run the command and capture its output
     THIS_OUTPUT=$(idf.py --version)
+fi
 
 if [[ "${THIS_KEYWORD}" != "CONNECT_ONLY" ]]; then
     echo "Build!"
